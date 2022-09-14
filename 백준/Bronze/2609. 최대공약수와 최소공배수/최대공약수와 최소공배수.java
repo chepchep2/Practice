a06bd7e88465db48a06b4d6df2449b187c85adfa
chepchep2 <100519505+chepchep2@users.noreply.github.com>
2022-09-15 00:25:25 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        int N = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int max = 0;
+
+        for (int i = 1; i <= a && i <= b; i++) {
+            if (a % i == 0 && b % i == 0) {
+                max = i;
+            }
+        }
+        System.out.println(max);
+
+        int min = (a * b) / max;
+
+        System.out.println(min);
+    }
+}
