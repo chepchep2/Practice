434887cbd110aca16eb6ea44cdfca9b788e9abca
chepchep2 <100519505+chepchep2@users.noreply.github.com>
2022-10-11 23:27:49 +0900

    [Bronze I] Title: 더하기 사이클, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int cnt = 0;
+        int copy = N;
+
+        while (true) {
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+            cnt++;
+
+            if (copy == N) {
+                break;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
+
