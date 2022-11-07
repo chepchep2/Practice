f41242ce3f771b64dcff619d4f464ab6178b8170
chepchep2 <100519505+chepchep2@users.noreply.github.com>
2022-11-07 21:46:12 +0900

    [Bronze II] Title: 나머지, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,21 +1,18 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 
 public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int arr[] = new int[10];
+        int arr[] = new  int[10];
         HashSet<Integer> set = new HashSet<>();
-        
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
 
             set.add(arr[i] % 42);
-            
         }
         System.out.println(set.size());
     }
 }
-
